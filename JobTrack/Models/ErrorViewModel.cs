1d85cf5c2b7898e85763617c2edfb0e272fba1e2
Michael Roberts <35013526+MDR88@users.noreply.github.com>
2018-10-26 15:05:25 -0500

    Installed the Entity Framework Core.

---
+++ JobTrack/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JobTrack.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
