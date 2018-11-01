a6435acacd7b16ebd0aefb4badcdaec31b04fd4c
Michael Roberts <35013526+MDR88@users.noreply.github.com>
2018-11-01 13:58:38 -0500

    Created a Create Job Link in the Job Create View Model.

--- JobTrack/ViewModels/JobEditViewModel.cs
+++ JobTrack/ViewModels/JobEditViewModel.cs
@@ -1,4 +1,6 @@
-﻿using JobTrack.Models;
+﻿using JobTrack.Data;
+using JobTrack.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,23 @@ namespace JobTrack.ViewModels
 {
     public class JobEditViewModel
     {
+
+        public List<SelectListItem> Status { get; set; }
+        public List<SelectListItem> Company { get; set; }
+
+        public JobCreateViewModel(ApplicationDbContext context)
+        {
+            Company = context.Company.Select(Company =>
+           new SelectListItem { Text = Company.Name, Value = Company.CompanyId.ToString() }).ToList();
+
+
+            Status = context.Status.Select(Status =>
+           new SelectListItem { Text = Status.Name, Value = Status.StatusId.ToString() }).ToList();
+
+        }
+
+
+
         public Job Job { get; set; }
     }
 }
